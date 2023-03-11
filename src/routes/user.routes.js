6306ac32376a527fc120a8dd24810df2e48face4
seb475 <111101299+seb475@users.noreply.github.com>
2023-03-11 14:20:12 -0600

    firts commit

---
+++ src/routes/user.routes.js
@@ -0,0 +1,27 @@
+const { getAll, create, getOne, remove, update, verifyEmail, login, getLoggedUser } = require('../controllers/user.controller');
+const express = require('express');
+const verifyJWT = require('../utils/verifyJWT');
+
+const userRouter = express.Router();
+
+userRouter.route('/')
+    .get(verifyJWT,getAll)
+    .post(create);
+
+userRouter.route('/verify/:code')
+    .get(verifyEmail)
+
+userRouter.route('/login')
+    .post(login)
+
+userRouter.route('/me')
+    .get(verifyJWT,getLoggedUser)
+
+userRouter.route('/:id')
+    .get(verifyJWT,getOne)
+    .delete(verifyJWT,remove)
+    .put(verifyJWT,update);
+
+   
+
+module.exports = userRouter;
